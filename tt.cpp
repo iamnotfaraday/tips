4206d5af839bbbd535270e7665ee08a7da8c7727
iamnotfaraday <2908213462@qq.com>
2024-12-29 19:00:37 +0800

    dsu add tt

--- tt.cpp
+++ tt.cpp
@@ -4,7 +4,37 @@ using namespace std;
 typedef long long ll;
 const int INF = 0x3f3f3f3f;
 
+struct DSU {
+    // 用法: DSU dsu(n);
+    std::vector<int > s, sz1, sz2; // sz1并查集点的数量, sz2边的数量
+    DSU(int n) : s(n + 1), sz1(n + 1, 1), sz2(n + 1, 0) {
+        iota(s.begin(), s.end(), 0);
+    }
+
+    int find(int x) {
+        return x == s[x] ? x : s[x] = find(s[x]);
+    }
+
+    bool merge(int x, int y) {
+        int xx = find(x), yy = find(y);
+        // 以xx作为父亲, 那么xx下的边数增加1
+        sz2[xx] += 1;
+        if(xx == yy) {
+            return false;
+        }
+        // 这里判断一下大小, 我们将小的给大的
+        if(sz1[xx] < sz2[yy]) {
+            std::swap(xx, yy);
+        }
+        sz1[xx] += sz1[yy];
+        sz2[xx] += sz2[yy];
+        s[yy] = xx;
+        return true;
+    }
+};
+
+
 int main() {
     IOS
-
+    
 }
