c76c252663cca7647fa7b269d9d8ca09f85ece9c
iamnotfaraday <2908213462@qq.com>
2024-12-29 18:48:02 +0800

    tt.cpp

---
+++ tt.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+
+int main() {
+    IOS
+
+}
